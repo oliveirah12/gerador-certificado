49964b96580f020753d8737776f2265412ce1933
Matheus_Oliveira_Cne <matheus@cnesistemas.com.br>
2025-09-17 14:35:58 -0300

    secondary button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { PrimaryButtonComponent } from './components/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from './components/secondary-button/secondary-button.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimaryButtonComponent],
+  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
