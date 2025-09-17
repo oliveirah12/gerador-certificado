a205483bc6bfe1614d80afbf3d7a223907b865d4
Matheus_Oliveira_Cne <matheus@cnesistemas.com.br>
2025-09-17 16:30:28 -0300

    certificate items list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
-import { PrimaryButtonComponent } from './components/primary-button/primary-button.component';
+import { PrimaryButtonComponent } from "./components/primary-button/primary-button.component";
 import { SecondaryButtonComponent } from './components/secondary-button/secondary-button.component';
+import { ItemCertificadoComponent } from './components/item-certificado/item-certificado.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/item-certificado/item-certificado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from '../secondary-button/secondary-button.component';
+@Component({
+  selector: 'app-item-certificado',
+  imports: [SecondaryButtonComponent],
+  templateUrl: './item-certificado.component.html',
+  styleUrl: './item-certificado.component.css'
+})
+export class ItemCertificadoComponent {
+
+}
