50b64010085a1d2d4d683e72e9ab5b1d89885803
Matheus_Oliveira_Cne <matheus@cnesistemas.com.br>
2025-09-18 16:41:14 -0300

    certificate list

--- src/app/pages/certificates/certificates.component.ts
+++ src/app/pages/certificates/certificates.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../../components/secondary-button/secondary-button.component";
+import { ItemCertificadoComponent } from "../../components/item-certificado/item-certificado.component";
 
 @Component({
   selector: 'app-certificates',
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, ItemCertificadoComponent],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.css'
 })
